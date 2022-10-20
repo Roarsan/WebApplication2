3b8bffc1f5591eabce771d25bc536e71023ef6eb
Roshan Gauchan <rgauchan3@gmail.com>
2022-10-20 11:43:23 +0100

    wa 2

---
+++ WebApplication2/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Movie
+    {
+        [Key]       
+           
+        public int Id { get; set; }
+        [Required, StringLength(20)]
+        public string Title { get; set; } = string.Empty;
+
+            [DataType(DataType.Date)]
+            
+        public DateTime ReleaseDate { get; set; }
+        [StringLength(20)]
+        public string? Genre { get; set; }
+        [Range(0,20)]
+        public decimal Price { get; set; } = 0;
+        
+    }
+}
